471191660f49da7e80136779c9bee7e4bf5a8620
cjd6568358 <cjd6568358@163.com>
2024-09-24 11:46:13 +0800

    fix

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,8 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import { createStyleImportPlugin, VantResolve } from 'vite-plugin-style-import'
 import { VitePWA } from 'vite-plugin-pwa';
+import fs from 'fs';
+import path from 'path';
 const INVALID_CHAR_REGEX = /[\x00-\x1F\x7F<>*#"{}|^[\]`;?:&=+$,]/g;
 const DRIVE_LETTER_REGEX = /^[a-z]:/i;
 // https://vitejs.dev/config/
@@ -56,7 +58,23 @@ export default defineConfig({
           resolveStyle: (name) => `../es/${name}/style` // 注意这里的路径适用于vite-plugin-style-import 2.0版本
         },
       ]
-    })
+    }),
+    {
+      name: 'copy-404',
+      closeBundle () {
+        const srcPath = path.resolve(__dirname, 'docs', 'index.html');
+        const destPath = path.resolve(__dirname, 'docs', '404.html');
+
+        // 复制 index.html 到 404.html
+        fs.copyFile(srcPath, destPath, (err) => {
+          if (err) {
+            console.error('生成 404.html 失败:', err);
+          } else {
+            console.log('成功生成 404.html');
+          }
+        });
+      },
+    },
   ],
   resolve: {
     extensions: [".js", ".jsx", ".ejs", '.mjs'], // 之前忽略了 .mjs
