b96cd95a3e55ee90d4fec2a54b75b34083ad5471
cjd6568358 <cjd6568358@163.com>
2024-09-24 11:30:07 +0800

    fix

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,26 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import { createStyleImportPlugin, VantResolve } from 'vite-plugin-style-import'
 import { VitePWA } from 'vite-plugin-pwa';
-
+const INVALID_CHAR_REGEX = /[\x00-\x1F\x7F<>*#"{}|^[\]`;?:&=+$,]/g;
+const DRIVE_LETTER_REGEX = /^[a-z]:/i;
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/smart-iot/",
   build: {
     outDir: "docs",
+    rollupOptions: {
+      output: {
+        // https://github.com/rollup/rollup/blob/master/src/utils/sanitizeFileName.ts
+        sanitizeFileName (fileName) {
+          const match = DRIVE_LETTER_REGEX.exec(fileName);
+          const driveLetter = match ? match[0] : "";
+          return (
+            driveLetter +
+            fileName.slice(driveLetter.length).replace(INVALID_CHAR_REGEX, "")
+          );
+        },
+      },
+    },
   },
   plugins: [
     // VitePWA({
